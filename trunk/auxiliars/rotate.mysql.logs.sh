41cb23ca8a77c2f04c4dd5cf825010d42cc91382
jordi.prats@gmail.com <jordi.prats@gmail.com@66608a9a-0e00-6b1b-3bce-e85eaf67d4c8>
2011-03-03 12:46:44 +0000

    rotate amb procesat de slows

--- trunk/auxiliars/rotate.mysql.logs.sh
+++ trunk/auxiliars/rotate.mysql.logs.sh
@@ -1,27 +1,70 @@
 #!/bin/bash
 
-#set -xv
+if [ ! -f /etc/rotatemysql.conf ]
+then
+	echo falta config
+	exit 1
+fi
 
-#LOGNAME=$(ps -fea | grep [m]ysqld | sed 's/^.*slow_query_log_file=//' | sed 's/ .*//')
-LOGNAME=$(echo "show variables like 'slow_query_log_file';" | /usr/local/mysql/bin/mysql -u root -p$(cat /var/mysql/.mysql.root.pass) -N | tail -n1 | awk '{ print $NF }')
+source /etc/rotatemysql.conf
+
+if [ -z "$DEFAULT_MYSQL_MAIL" ]
+then
+	echo default mail
+	exit 2
+fi
+
+LOGNAME=$(ps -fea | grep [m]ysqld | sed 's/^.*slow_query_log_file=//' | sed 's/ .*//')
 
 if [ -z "$LOGNAME" ];
 then
         echo "ERROR: slowquery log NOT available"
 else
-        mv $LOGNAME $(dirname ${LOGNAME})/gi-$(basename $LOGNAME).$(date --date="1 week ago" +%Y%m%d)_$(date +%Y%m%d)
+        mv $LOGNAME $(dirname ${LOGNAME})/$(basename $LOGNAME).$(date +%Y%m%d)
 fi
 
-#LOGNAME=$(ps -fea | grep [m]ysqld | sed 's/^.*log-error=//' | sed 's/ .*//')
-LOGNAME=$(echo "show variables like 'log_error';" | /usr/local/mysql/bin/mysql -u root -p$(cat /var/mysql/.mysql.root.pass) -N | tail -n1 | awk '{ print $NF }')
+/usr/local/mysql/bin/mysqladmin -u root -p$(cat /var/mysql/.mysql.root.pass) flush-logs
+
+OLDLOG=$(dirname ${LOGNAME})/$(basename $LOGNAME).$(date +%Y%m%d)
+
+for i in $(cat $OLDLOG | grep "^# User" | awk '{ print $3 }' | cut -f1 -d[ | sort | uniq)
+do
+	for e in $(set | grep USER_MYSQL_)
+	do
+		if [ ${e##*=} == $i ]
+		then
+			#mailto?
+			USERMYSQLVAR=${e%%=*}
+			USERNUMVAR=${USERMYSQLVAR##*_}
+			MAILTO_SLOWS=$(set | grep MAIL_MYSQL_$MAILVAR)
+			MAILTO_SLOWS=${MAILTO_SLOWS##*=}
+
+			if [ -z "$MAILTO_SLOWS" ];
+			then
+				MAILTO_SLOWS=$DEFAULT_MYSQL_MAIL
+			fi
+
+			# procesa slowqueries
+			/usr/local/bin/mk-query-digest --filter  '($event->{user} || "") =~ m/'$i'/' $OLDLOG | mail -s "slow queries $i [$(date +%Y%m%d)]" $MAILTO_SLOWS
+		fi
+	done
+
+done
+
+#else
+for i in $(cat $OLDLOG | grep "^# User" | awk '{ print $3 }' | cut -f1 -d[ | sort | uniq | grep -v $(set | grep USER_MYSQL_ | awk -F= '{ print $2 }' | perl -pe 's/\n/\$\\|/' | sed 's#\\|$##'))
+do
+	/usr/local/bin/mk-query-digest --filter  '($event->{user} || "") =~ m/'$i'/' $OLDLOG | mail -s "slow queries $i@$HOSTNAME [$(date +%Y%m%d)]" $DEFAULT_MYSQL_MAIL
+done
+
+LOGNAME=$(ps -fea | grep [m]ysqld | sed 's/^.*log-error=//' | sed 's/ .*//')
 
 if [ -z "$LOGNAME" ];
 then
         echo "ERROR: general log NOT available"
 else
-        mv $LOGNAME $(dirname ${LOGNAME})/$(basename $LOGNAME).$(date --date="1 week ago" +%Y%m%d)_$(date +%Y%m%d)
+        mv $LOGNAME $(dirname ${LOGNAME})/$(basename $LOGNAME).$(date +%Y%m%d)
 fi
 
 /usr/local/mysql/bin/mysqladmin -u root -p$(cat /var/mysql/.mysql.root.pass) flush-logs
-
 
