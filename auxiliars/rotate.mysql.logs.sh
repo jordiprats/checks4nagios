3edf623dc519ec17e4057435163945159b4309ab
Jordi Prats <jprats@systemadmin.es>
2011-03-10 13:18:29 +0000

    varis rotate

--- auxiliars/rotate.mysql.logs.sh
+++ auxiliars/rotate.mysql.logs.sh
@@ -29,33 +29,42 @@ OLDLOG=$(dirname ${LOGNAME})/$(basename $LOGNAME).$(date +%Y%m%d)
 
 for i in $(cat $OLDLOG | grep "^# User" | awk '{ print $3 }' | cut -f1 -d[ | sort | uniq)
 do
-	for e in $(set | grep USER_MYSQL_)
-	do
-		if [ ${e##*=} == $i ]
-		then
-			#mailto?
-			USERMYSQLVAR=${e%%=*}
-			USERNUMVAR=${USERMYSQLVAR##*_}
-			MAILTO_SLOWS=$(set | grep MAIL_MYSQL_$MAILVAR)
-			MAILTO_SLOWS=${MAILTO_SLOWS##*=}
-
-			if [ -z "$MAILTO_SLOWS" ];
+	if [ -n "$(set | grep USER_MYSQL_)" ]
+	then
+		for e in $(set | grep USER_MYSQL_)
+		do
+			if [ ${e##*=} == $i ]
 			then
-				MAILTO_SLOWS=$DEFAULT_MYSQL_MAIL
+				#mailto?
+				USERMYSQLVAR=${e%%=*}
+				USERNUMVAR=${USERMYSQLVAR##*_}
+				MAILTO_SLOWS=$(set | grep MAIL_MYSQL_$MAILVAR)
+				MAILTO_SLOWS=${MAILTO_SLOWS##*=}
+	
+				if [ -z "$MAILTO_SLOWS" ];
+				then
+					MAILTO_SLOWS=$DEFAULT_MYSQL_MAIL
+				fi
+	
+				# procesa slowqueries
+				/usr/local/bin/mk-query-digest --filter  '($event->{user} || "") =~ m/'$i'/' $OLDLOG | mail -s "slow queries $i [$(date +%Y%m%d)]" $MAILTO_SLOWS
 			fi
+		done
+	fi
 
-			# procesa slowqueries
-			/usr/local/bin/mk-query-digest --filter  '($event->{user} || "") =~ m/'$i'/' $OLDLOG | mail -s "slow queries $i [$(date +%Y%m%d)]" $MAILTO_SLOWS
-		fi
+done
+
+if [ -z "$(set | grep USER_MYSQL_)" ]
+then
+	(cat $OLDLOG | grep "^# User" | awk '{ print $3 }' | cut -f1 -d[ | sort | uniq; /usr/local/bin/mk-query-digest $OLDLOG) | mail -s "slow queries - $HOSTNAME [$(date +%Y%m%d)]" $DEFAULT_MYSQL_MAIL
+else
+	for i in $(cat $OLDLOG | grep "^# User" | awk '{ print $3 }' | cut -f1 -d[ | sort | uniq | grep -v $(set | grep USER_MYSQL_ | awk -F= '{ print $2 }' | perl -pe 's/\n/\$\\|/' | sed 's#\\|$##'))
+	do
+		/usr/local/bin/mk-query-digest --filter  '($event->{user} || "") =~ m/'$i'/' $OLDLOG | mail -s "slow queries $i@$HOSTNAME [$(date +%Y%m%d)]" $DEFAULT_MYSQL_MAIL
 	done
+fi
 
-done
-
-#else
-for i in $(cat $OLDLOG | grep "^# User" | awk '{ print $3 }' | cut -f1 -d[ | sort | uniq | grep -v $(set | grep USER_MYSQL_ | awk -F= '{ print $2 }' | perl -pe 's/\n/\$\\|/' | sed 's#\\|$##'))
-do
-	/usr/local/bin/mk-query-digest --filter  '($event->{user} || "") =~ m/'$i'/' $OLDLOG | mail -s "slow queries $i@$HOSTNAME [$(date +%Y%m%d)]" $DEFAULT_MYSQL_MAIL
-done
+(echo "rules:"; echo "http://www.maatkit.org/doc/mk-query-advisor.html#rules"; echo ""; /usr/local/bin/mk-query-advisor $OLDLOG) | mail -s "query advisor $i [$(date +%Y%m%d)]" $ADVISOR_MAIL
 
 LOGNAME=$(ps -fea | grep [m]ysqld | sed 's/^.*log-error=//' | sed 's/ .*//')
 
@@ -67,4 +76,5 @@ else
 fi
 
 /usr/local/mysql/bin/mysqladmin -u root -p$(cat /var/mysql/.mysql.root.pass) flush-logs
+
 
