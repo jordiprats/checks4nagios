bb1d1cda6afcfb630b7f31f85fd4d71f5669cdae
jordi.prats@gmail.com <jordi.prats@gmail.com@66608a9a-0e00-6b1b-3bce-e85eaf67d4c8>
2013-12-02 11:09:05 +0000

    cosa del gerard

--- trunk/auxiliars/optimize.amb.percona.sh
+++ trunk/auxiliars/optimize.amb.percona.sh
@@ -2,7 +2,7 @@
 
 #cosa del gerard
 
-DBNAME="TODO!!!"
+DBNAME="ivoox"
 
 for last; do true; done
 EXECUTE=$last
@@ -13,9 +13,13 @@ EXECUTE=$last
 # SOME INNODB TABLES REQUIRE SPECIFIC OPTIMIZE TO SKIP THE LOCKING PROBLEM playlist comment viewstoday viewsweek viewsmonth
 # THE OTHER ONES ARE OPTIMIZED AS ALWAYS
 
+SKIP_TABLES=( \`activity_backup\` \`apptype\` \`audiographrelation\` \`clicksonrelatedaudios\` \`downloads\` \`downloadsHistory\` \`downloadsStats\` \`downloads_backuo2\` \`downloads_backup\` \`downloadsh\` \`downloadshour\` \`downloadshourtmp\` \`downloadstest\`  \`likegraph\` \`mailqueue_backup3\` \`referer\` \`statsDailyAudio\` \`statsDailyChannel\` \`statsDailyProgram\` \`statsDailyUser\` \`statsDailyUser2\` \`statsMonthlyChannel\` \`statsMonthlyProgram\` \`statsMonthlyUser\` \`statsRanking\` \`statsUpload\` \`subscriptionmail\` \`suscriptionunread3\` )
 
-TABLES_INNO=($(/usr/local/bin/mk-find -p $(cat /var/mysql/.mysql.root.pass) --engine=InnoDB --printf "%N\n" "$DBNAME" | tr -d '`'))
-TABLES_MY=($(/usr/local/bin/mk-find -p $(cat /var/mysql/.mysql.root.pass) --engine=MyISAM --printf "%N\n" "$DBNAME" | tr -d '`'))
+
+TABLES_INNO=($(/usr/local/bin/mk-find -p $(cat /var/mysql/.mysql.root.pass) --engine=InnoDB --printf "%N\n" "$DBNAME"))
+#TABLES_INNO=($(/usr/local/bin/mk-find -p $(cat /var/mysql/.mysql.root.pass) --engine=InnoDB --printf "%N\n" "$DBNAME" | sed 's/`/\\`/g'))
+#TABLES_MY=($(/usr/local/bin/mk-find -p $(cat /var/mysql/.mysql.root.pass) --engine=MyISAM --printf "%N\n" "$DBNAME" | sed 's/`/\\`/g'))
+TABLES_MY=($(/usr/local/bin/mk-find -p $(cat /var/mysql/.mysql.root.pass) --engine=MyISAM --printf "%N\n" "$DBNAME"))
 
 TEMPF=$(mktemp /tmp/$(basename $0).XXXXXXXXXX)
 
@@ -23,7 +27,10 @@ TEMPF=$(mktemp /tmp/$(basename $0).XXXXXXXXXX)
 for i in "${TABLES_INNO[@]}"
 do
 
-   if [ "${i}" = "playlist" ] || [ "${i}" = "comment" ] || [ "${i}" = "viewstoday" ] || [ "${i}" = "viewsweek" ] || [ "${i}" = "viewsmonth" ]; then
+   # Busquem l'element $i i si existeix retornem un array sense aquest element. Sinó tornem el mateix element.
+   CERCA=( $(echo ${SKIP_TABLES[@]##$i}) )
+
+   if [ ${#SKIP_TABLES[@]} -eq  ${#CERCA[@]} ]; then #vol dir que NO l'ha trobat a la llista de taules que no hem de fer. Per tant l'hem d'optimitzar
 
       ARE_FK_REFERENCED=""
 
@@ -33,9 +40,13 @@ do
       if [ -n "$ARE_FK_REFERENCED" ]; then
          echo "${i} HAS NOT BEEN OPTIMIZED. CHECK TABLE BECAUSE HAS FK REFERENCED AND YOU HAVE TO BE CAREFULL RUNNING pt-online-schema-change (--alter-foreign-keys-method)"
       else
+         LENGTH=$((${#i}-2))
          INICI=$(date +'%s')
          if [ "$EXECUTE" = "optimize" ]; then
-            /usr/local/bin/pt-online-schema-change --password=$(cat /var/mysql/.mysql.root.pass) --alter "ENGINE=InnoDB" D=ivoox,t="${i}" --execute >> $TEMPF
+            #hi ha un problema que no es menja bé les cometes `taula` per solucionar-ho les trec. Té cert sentit perquè hi poden haver taules que es diguin '`taul`a'
+            /usr/local/bin/pt-online-schema-change --password=$(cat /var/mysql/.mysql.root.pass) --alter "ENGINE=InnoDB" D=$DBNAME,t="${i:1:$LENGTH}" --execute >> $TEMPF
+         #else # NO FA RES ES UN SIMULACRE
+         #   /usr/local/bin/pt-online-schema-change --password=$(cat /var/mysql/.mysql.root.pass) --alter "ENGINE=InnoDB" D=ivoox,t="${i:1:$LENGTH}"
          fi
          FI=$(date +'%s')
 
@@ -43,13 +54,15 @@ do
       fi
 
    else
-      INICI=$(date +'%s')
-      if [ "$EXECUTE" = "optimize" ]; then
-         /usr/local/mysql/bin/mysql -p$(cat /var/mysql/.mysql.root.pass) -e "optimize table ${i}" $DBNAME >> $TEMPF
-      fi
-      FI=$(date +'%s')
+      SKIP_TABLES=( $(echo ${CERCA[@]}) )
+      printf "%-28s %-30s %-35s\n" " " "${i}" "Skipping..."
+#      INICI=$(date +'%s')
+#      if [ "$EXECUTE" = "optimize" ]; then
+#         /usr/local/mysql/bin/mysql -p$(cat /var/mysql/.mysql.root.pass) -e "optimize table ${i}" $DBNAME >> $TEMPF
+#      fi
+#      FI=$(date +'%s')
 
-      printf "%-28s %-30s %-35s\n" "Inici: $(date -d@$INICI +'%D-%T')" "${i}" "Temps total: $(($FI-$INICI)) segons"
+#      printf "%-28s %-30s %-35s\n" "Inici: $(date -d@$INICI +'%D-%T')" "${i}" "Temps total: $(($FI-$INICI)) segons"
    fi
 done
 
@@ -57,16 +70,23 @@ done
 
 for i in "${TABLES_MY[@]}"
 do
-   INICI=$(date +'%s')
-   if [ "$EXECUTE" = "optimize" ]; then
-      /usr/local/mysql/bin/mysql -p$(cat /var/mysql/.mysql.root.pass) -e "optimize table ${i}" $DBNAME >> $TEMPF
+   CERCA=( $(echo ${SKIP_TABLES[@]##$i}) )
+
+   if [ ${#SKIP_TABLES[@]} -eq  ${#CERCA[@]} ]; then #vol dir que NO l'ha trobat a la llista de taules que no hem de fer. Per tant l'hem d'optimitzar
+      INICI=$(date +'%s')
+      if [ "$EXECUTE" = "optimize" ]; then
+         LENGTH=$((${#i}-2))
+         /usr/local/mysql/bin/mysql -p$(cat /var/mysql/.mysql.root.pass) -e "optimize table ${i:1:$LENGTH}" $DBNAME >> $TEMPF
+      fi
+      FI=$(date +'%s')
+      printf "%-28s %-30s %-35s\n" "Inici: $(date -d@$INICI +'%D-%T')" "${i}" "Temps total: $(($FI-$INICI)) segons"
+   else
+      SKIP_TABLES=( $(echo ${CERCA[@]}) )
+      printf "%-28s %-30s %-35s\n" " " "${i}" "Skipping..."
    fi
-   FI=$(date +'%s')
-   printf "%-28s %-30s %-35s\n" "Inici: $(date -d@$INICI +'%D-%T')" "${i}" "Temps total: $(($FI-$INICI)) segons"
 done
 
 echo -ne "\n\n\n"
 
 cat $TEMPF
-rm $TEMPF
 
